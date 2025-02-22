873e2a6d81b2cfe7a3a4379440f1a5fd0c28b005
dioradelvina03 <dioradelvina03@gmail.com>
2025-02-22 14:58:12 +0100

    task seven

---
+++ src/main/java/exercise7/Main.java
@@ -0,0 +1,27 @@
+/*Create Employee class with id, name and salary fields. Create necessary methods and
+constructor. Create 3 instances of the Employee class and add them to a list. Using
+streams, find first Employee that is not null and has salary of at least 30000. If not found,
+throw exception.*/
+package exercise7;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee employee1 = new Employee(1, "John", 1800);
+        Employee employee2 = new Employee(2, "Jane", 30000);
+        Employee employee3 = new Employee();
+
+        List<Employee> employeeList = Arrays.asList(employee1, employee2, employee3);
+
+        Employee employee = employeeList.stream()
+                .filter(e -> e != null)
+                .filter(e -> e.salary >= 30000)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("No employee found with salary >= 30000"));
+
+        System.out.println(employee);
+    }
+}
